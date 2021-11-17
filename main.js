ef048e31a10da9eea34d14119eb647fa8f2a852e
alaina-khan <92048635+alaina-khan@users.noreply.github.com>
2021-11-17 16:18:10 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function setup() {
+    canvas= createCanvas(280, 280);
+    canvas.center();
+    background("white");
+}
+
+function clearCanvas()
+{
+    background("white");
+}
+
+canvas
+{
+    border:5px solid white;
+    border-radius: 20 px;
+    animation-name: example;
+    animation-duration: 4s;
+    margin-top: 50px;
+    animation-iteration-count:infinite;
+
+
+}
+
+    @keyframes example {
+        from {box-shadow: 1px 1px 38px Grey;}
+        to {box-shadow: 1px 1px 38px White;}
+
+    }
+
+    p
+    {
+        padding-top: 10px;
+        font-size: 25px;
+    
+    }
